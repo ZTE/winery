307bb985dddf1a79975fccd4a495888ed742bd41
Oliver Kopp <kopp.dev@gmail.com>
2017-01-15 23:10:53 +0100

    First shot for the implementation

--- org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/splitting/ProviderRepository.java
+++ org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/splitting/ProviderRepository.java
@@ -13,45 +13,77 @@
 package org.eclipse.winery.repository.splitting;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.xml.namespace.QName;
 
+import org.eclipse.winery.common.ids.Namespace;
 import org.eclipse.winery.common.ids.definitions.RequirementTypeId;
+import org.eclipse.winery.common.ids.definitions.ServiceTemplateId;
 import org.eclipse.winery.model.tosca.TNodeTemplate;
 import org.eclipse.winery.model.tosca.TRequirement;
+import org.eclipse.winery.repository.backend.Repository;
 import org.eclipse.winery.repository.resources.AbstractComponentsResource;
 import org.eclipse.winery.repository.resources.entitytypes.requirementtypes.RequirementTypeResource;
+import org.eclipse.winery.repository.resources.servicetemplates.ServiceTemplateResource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProviderRepository {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProviderRepository.class);
 
 	public static final ProviderRepository INSTANCE = new ProviderRepository();
 
+	private static final Map<String, Namespace> targetLocationToNamespaceMap = new HashMap<>();
+	static {
+		targetLocationToNamespaceMap.put("AmazonPaaS", new Namespace("http://www.opentosca.org/providers/amazon/paas", false));
+		targetLocationToNamespaceMap.put("OnPremiseIAAS", new Namespace("http://www.opentosca.org/providers/IAAS/", false)); // Alternative namespace: http://www.iaas.uni-stuttgart.de/onpremise
+		targetLocationToNamespaceMap.put("OnPremiseIPVS", new Namespace("http://www.opentosca.org/providers/IPVS/", false));
+	}
+
 	/**
+	 * TODO: This implements a collection of node templates only. One could also point directly to a certain node tempalte.
+	 *       Current workaround: Create a service template in a separeate namespace containing a single node template only
+	 *
 	 * @return All node templates available for the given targetLocation
 	 */
 	public List<TNodeTemplate> getAllNodeTemplatesForLocation(String targetLocation) {
-		return Collections.emptyList();
+		Namespace namespace = targetLocationToNamespaceMap.get(targetLocation);
+		if (namespace == null) {
+			LOGGER.info("unknown targetLocation " + targetLocation);
+			return Collections.emptyList();
+		}
+		return Repository.INSTANCE.getAllTOSCAComponentIds(ServiceTemplateId.class).stream()
+				// get all service templates in the namespace
+				.filter(id -> id.getNamespace().equals(namespace))
+				// get all contained node templates
+				.flatMap(id -> {
+					ServiceTemplateResource serviceTemplateResource = (ServiceTemplateResource) AbstractComponentsResource.getComponentInstaceResource(id);
+					return serviceTemplateResource.getServiceTemplate().getTopologyTemplate().getNodeTemplateOrRelationshipTemplate().stream()
+							.filter(t -> t instanceof TNodeTemplate)
+							.map(TNodeTemplate.class::cast);
+				})
+				.collect(Collectors.toList());
 	}
 
 	/**
 	 * @return All node templates available for the given targetLocation
 	 */
 	public List<TNodeTemplate> getAllNodeTemplatesForLocationAndOfferingCapability(String targetLocation, List <TRequirement> requirements) {
-
 		QName reqTypeQName = requirements.get(0).getType();
 		RequirementTypeId reqTypeId = new RequirementTypeId(reqTypeQName);
 		RequirementTypeResource reqTypeResource = (RequirementTypeResource) AbstractComponentsResource.getComponentInstaceResource(reqTypeId);
-		reqTypeResource.getRequirementType().getRequiredCapabilityType();
+		QName requiredCapabilityType = reqTypeResource.getRequirementType().getRequiredCapabilityType();
 
-		/*List<TNodeTemplate> allNodeTemplatesForLocation = this.getAllNodeTemplatesForLocation(targetLocation);
-		List<QName> allNodeTypesOfferingCapability = Query.INSTANCE.getAllNodeTypesOfferingCapability(requirements)
-				.stream()
-				.map(t -> new QName(t.getTargetNamespace(), t.getName()) )
-				.collect(Collectors.toList());
-		List<TNodeTemplate> result = allNodeTemplatesForLocation.stream().filter(nt -> allNodeTypesOfferingCapability.contains(nt.getType())).collect(Collectors.toList());
-		return result;*/
-		return Collections.emptyList();
+		return this.getAllNodeTemplatesForLocation(targetLocation).stream()
+				.filter(nt -> nt.getCapabilities() != null)
+				.filter(nt -> nt.getCapabilities().getCapability().stream()
+						.anyMatch(cap -> cap.getType().equals(requiredCapabilityType))
+				).collect(Collectors.toList());
 	}
 
 }
