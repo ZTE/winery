25726af1c4db067da1bb944646ba71d0ff503699
吕波 <lv.bo163@zte.com.cn>
2017-04-12 16:54:57 +0800

    change the first and last line of copyright info. for tslint complains aout all the stars.
    Signed-off-by: 吕波 <lv.bo163@zte.com.cn>

--- org.eclipse.winery.workflowmodeler/src/app/services/winery.service.ts
+++ org.eclipse.winery.workflowmodeler/src/app/services/winery.service.ts
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/**
  * Copyright (c) 2017 ZTE Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,7 +8,7 @@
  *
  * Contributors:
  *     ZTE - initial API and implementation and/or initial documentation
- *******************************************************************************/
+ */
 
 import {Injectable} from '@angular/core';
 import {isNullOrUndefined} from 'util';
